0dde965f2c20a4d321d4bd325e450d227e4b4ea4
Zhang Yuntong <ang.unong@gmail.com>
2022-07-11 09:33:16 +0800

    Backup fix file for validation as well

--- src/validate.py
+++ src/validate.py
@@ -10,9 +10,13 @@ def validate_patch(patch_file):
     if not values.all_fail_inputs:
         return
 
-    # restore project state if there is a backup file
     if os.path.isfile(values.backup_file_path):
+        # restore project state if there is a backup file
         restore_orig_patch_file()
+    else:
+        # copy file to backup if there is not one
+        shutil.copy2(values.fix_file_path, values.backup_file_path)
+
 
     # apply given patch file
     apply_patch_file(patch_file)
@@ -32,6 +36,15 @@ def gather_failing_inputs():
         return
     afl_fail = os.listdir(values.dir_afl_fail)
     values.all_fail_inputs = sorted([ os.path.join(values.dir_afl_fail, t) for t in afl_fail ])
+
+
+def gather_passing_inputs():
+    if values.all_pass_inputs:
+        return
+    if not os.path.isdir(values.dir_afl_pass):
+        return
+    afl_pass = os.listdir(values.dir_afl_pass)
+    values.all_pass_inputs = sorted([ os.path.join(values.dir_afl_pass, t) for t in afl_pass ])
 
 
 def apply_patch_file(patch_file):
